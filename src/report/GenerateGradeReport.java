807ffa4451bb99b81f761349cb05c84d0324caa1
류나연 <helloyoni1771@gmail.com>
2021-10-23 17:08:22 +0900

    학점정책, school클래스

---
+++ src/report/GenerateGradeReport.java
@@ -0,0 +1,14 @@
+package report;
+
+import grade.BasicEvaluation;
+import grade.GradeEvaluation;
+import grade.MajorEvaluation;
+import school.School;
+import school.Subject;
+import school.Student;
+import school.Score;
+import utils.Define;
+
+public class GenerateGradeReport {
+    School school = School.getInstance();
+}
